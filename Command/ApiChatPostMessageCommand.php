092eaed196d7f16290353d8af421cb076c2201e7
Cas Leentfaar <git@casleentfaar.com>
2014-07-02 00:12:22 +0200

    fixed chat command

--- Command/ApiChatPostMessageCommand.php
+++ Command/ApiChatPostMessageCommand.php
@@ -32,8 +32,8 @@ class ApiChatPostMessageCommand extends AbstractApiCommand
         $this->addArgument('channel', InputArgument::REQUIRED, 'The channel to send the text to');
         $this->addArgument('text', InputArgument::REQUIRED, 'The text to send');
         $this->addOption('username', 'u', InputOption::VALUE_REQUIRED, 'The username that will send this text');
-        $this->addOption('icon_url', null, InputOption::VALUE_REQUIRED, 'The URL to use for showing an icon next to the text');
-        $this->addOption('icon_emoji', null, InputOption::VALUE_REQUIRED, 'The Slack icon to use next to the text, overrides icon_url');
+        $this->addOption('icon-url', null, InputOption::VALUE_REQUIRED, 'The URL to use for showing an icon next to the text');
+        $this->addOption('icon-emoji', null, InputOption::VALUE_REQUIRED, 'The Slack icon to use next to the text, overrides icon_url');
     }
 
     /**
@@ -46,8 +46,8 @@ class ApiChatPostMessageCommand extends AbstractApiCommand
         $options               = parent::inputToOptions($input);
         $options['channel']    = '#' . ltrim($input->getArgument('channel'), '#');
         $options['text']       = $input->getArgument('text');
-        $options['icon_emoji'] = (string) $input->getOption('icon_emoji');
-        $options['icon_url']   = (string) $input->getOption('icon_url');
+        $options['icon_url']   = (string) $input->getOption('icon-url');
+        $options['icon_emoji'] = $input->hasOption('icon-emoji') ? ':' . trim($input->getOption('icon-emoji'), ':') . ':' : '';
         $options['username']   = (string) $input->getOption('username');
 
         return $options;
